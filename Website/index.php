c2179d1d56afc33f69cfa350530825412e84dd11
ShaneTim <Shane.Timothy.Reeves@hotmail.com>
2015-01-23 20:27:31 +1100

    Added Data Validation

--- Website/index.php
+++ Website/index.php
@@ -18,10 +18,14 @@
 		<link href="/styles/sticky-footer-navbar.css" rel="stylesheet">
 		<link href="/styles/theme.css" rel="stylesheet">
 		<link href="/styles/bootstrap-dialog.css" rel="stylesheet">
+		<link href="/styles/bootstrap-switch.css" rel="stylesheet">
 
 		<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
 		<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 		<script src="/bootstrap/js/ie-emulation-modes-warning.js"></script>
+
+		<!-- JavaScript for form -->
+		<script src="/scripts/loan-calculator.js" type="text/javascript"></script>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -68,6 +72,22 @@
 
 		<!-- Begin page content -->
 		<div class="container">
+			<div id="welcome">
+				<h1>Welcome to Finance Made Simple</h1>
+				<p>Hi there, my name is Cameron Martin and welcome to financemadesimple.net! For most of us the purchase of a home will likely be the largest financial decision we ever 
+				make. With this in mind, it makes sense to know as many of the ins and outs of a home loan as possible so you can achieve the best financial outcome for you and your 
+				family.</p>
+				<p>While home loan calculators already exist, they are often simple, not taking into account paying more than you need to, and being tailored to people looking to get a 
+				home loan with little attention paid to those with existing loans. Not to worry however, my calculators will let you see how much your duration decreases by paying more 
+				per week/fortnight/month, and it works with existing loans too!</p>
+				<p>Even if you have a fixed interest period, you'll be able to enter your details and assess your payment options once the fixed interest period expires.</p>
+				<p>Disclaimer: Please note that this website is currently in development and undergoing testing, figures may not be 100% accurate at all times. You should seek 
+				professional advice before acting on any of the imformation seen on this site.</p>
+				<br />
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4" style="text-align: center;"><button class="btn btn-success btn-lg" onclick="start();">Get Started</button></div>
+				</div>
+			</div>
 			<form id="main-form" action="" method="post" onsubmit="validateInput()">
 				<div id="form-steps">
 					<div class="form-step">
@@ -78,35 +98,234 @@
 						please enter the amount you borrowed at the very begining of your loan, this calculator will determine your remaining present value behind the scenes and give you 
 						a meaningful result if you change your payment amount or interest rate.</p><br />
 						<div class="row">
-						    <div class="col-md-4 col-md-offset-4">
+							<div class="col-md-4 col-md-offset-4">
 								<div class="input-group">
 									<span class="input-group-addon">$</span>
-									<input type="number" class="form-control" id="borrow-amount" placeholder="Enter a figure" min="1" required />
+									<input type="number" class="form-control" id="borrow-amount" placeholder="Enter a figure" min="1" onkeyup="checkForReturn(event)" required />
 									<span class="input-group-addon">.00</span>
 								</div>
-						    </div>
+							</div>
 						</div>
 					</div>
-					<div class="form-step" style="display: none;">
+					<div class="form-step">
 						<div class="page-header">
 							<h1>How many years have you had the loan for?</h1>
 						</div>
 						<p>If you have an existing loan you can use this calcutor to see how changes in various factors will influence the rest of your loan.<br><br> Please enter the number 
 						of years you've had your current loan, if you don't have a current loan, please enter a zero.</p><br />
 						<div class="row">
-						    <div class="col-md-2 col-md-offset-5">
+							<div class="col-md-2 col-md-offset-5">
 								<input type="number" class="form-control" id="loan-years" placeholder="Years" min="0" required />
-						    </div>
+							</div>
 						</div>
 					</div>
-				</div><br /><br />
-				<div class="row">
-					<div class="col-md-6 half"><button type="button" class="btn btn-primary btn-large" onclick="previous();" id="previous-button" disabled="true">Previous</button></div>
-					<div class="col-md-6 half" style="text-align: right;"><button type="button" class="btn btn-success btn-large" onclick="next();" id="next-button">Next</button></div>
-				</div><br />
-				<div class="progress">
-					<div id="progress-bar" class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
-						<span class="sr-only">0% Complete</span>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>Is there a fixed period?</h1>
+						</div>
+						<p>This is the period of time in which the interest rate is static, it doesn't change even if the bank changes their rates, you're locked into this rate for this 
+						duration. It is sometimes associated with periods of time in which you're unable to pay more than your regular payment, this calculator makes use of the assumption.</p>
+						<p>Please enter the number of years you've had your current loan, if you don't have a current loan, please enter a zero.</p><br />
+						<div class="row">
+							<div class="col-md-2 col-md-offset-5">
+								<input type="number" class="form-control" id="fixed-period" placeholder="Years" min="0" required />
+							</div>
+						</div>
+					</div>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>What is the fixed interest period's rate? (%)</h1>
+						</div>
+						<p>This is your standard fixed interest rate, they may vary from institution from institution. I encourage you to look around, you may be able to find a lower rate, 
+						and therefore pay less interest! </p>
+						<p>If there is no fixed period, make this field exactly the same as the variable interest rate two boxes below.</p><br />
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								<div class="input-group">
+									<input type="number" class="form-control" id="fixed-interest-rate" min="0" max="100" step="0.01" required />
+									<span class="input-group-addon">%</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>What is the total length of the loan? (Years)</h1>
+						</div>
+						<p>This is the duration or length of the loan, it is measured in years. This is how long you'd like to pay off the loan for. You'll notice that if you choose to 
+						include an additional payment this number will decrease, meaning you'll pay your loan off quicker and with less interest!</p><br />
+						<div class="row">
+							<div class="col-md-2 col-md-offset-5">
+								<input type="number" class="form-control" id="loan-length" placeholder="Years" min="1" required />
+							</div>
+						</div>
+					</div>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>What is the variable interest rate?</h1>
+						</div>
+						<p>This is the variable interest rate, it is able to fluctate and most likely will over the duration of your home loan. The higher this percentage is, the more 
+						money you will have to pay your financial institution in interest repayments. It is determined by the reserve bank and there a many complicated factors that decide 
+						what this rate is. It is beyond your control and something that must simply be accepted.</p><br />
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								<div class="input-group">
+									<input type="number" class="form-control" id="variable-interest-rate" min="0" max="100" step="0.01" required />
+									<span class="input-group-addon">%</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>After the fixed interest period ends, how much would you like to pay?</h1>
+						</div>
+						<p>The most important function of the calculator and also the reason financemadesimple exists! This is the value of the extra money you would pay weekly. If you've 
+						decided you can afford to pay an additional $50 per period towards your loan, please enter 50. Paying extra per period reduces the present value of your loan faster 
+						than you would if you paid the regular amount, what that means is, the total amount owing is decreasing faster.</p>
+						<p>This will decrease the number of periods required to pay back your loan, reducing both the total time it would take to pay off your loan, as well as reducing the 
+						total interest you pay your financial instituion. Paying as little as $50 extra per week could save you literally tens of thousands of dollars of the duration of the 
+						loan.</p>
+						<p> This will decrease the number of periods required to pay back your loan, reducing both the total time it would take to pay off your loan, as well as reducing the 
+						total interest you pay your financial instituion. Paying as little as $50 extra per week could save you literally tens of thousands of dollars of the duration of the 
+						loan.</p><br />
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								<div class="input-group">
+									<span class="input-group-addon">$</span>
+									<input type="number" class="form-control" id="pay-at-end" placeholder="Enter a figure" min="1" required />
+									<span class="input-group-addon">.00</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-step">
+						<div class="page-header">
+							<h1>Lastly, please select the payment frequency of your payments</h1>
+						</div>
+						<p>This is the duration or length of the loan, it is measured in years. This is how long you'd like to pay off the loan for. You'll notice that if you choose to 
+						include an additional payment this number will decrease, meaning you'll pay your loan off quicker and with less interest!</p><br />
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								<select class="form-control" id="payment-frequency" required>
+									<option value="52" selected>Weekly</option>
+									<option value="26">Fortnightly</option>
+									<option value="12">Monthly</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="results">
+					<div class="page-header">
+						<h1>Results</h1>
+					</div>
+					<table class="table table-hover">
+						<tr onclick="$('#results-periodic-payment-info').fadeToggle(200);">
+							<th>Periodic payment</th>
+							<td id="results-periodic-payment">$100</td>
+						</tr>
+						<tr id="results-periodic-payment-info">
+							<td colspan="2">
+								<p>This figure represents your on going payment. So whatever frequency you selected (Weeks, Months, Fortnights), this is the value you will be required to pay each 
+								week/fortnight/month.</p>
+								<p>A very important thing to note is that the interest component of this figure is calculated on your total remaining principal (The amount you borrowed minus what 
+								you've paid back.)</p>
+								<p>A simple example: If you borrowed $500,000 over 25 years at 5% per annum and only had to pay once a year, each payment would be worth $35,476.23. The interest on 
+								$500,000 at 5% is $25,000 though, so $34,476.24-$25,000= $9,476.</p>
+								<p>So after your first payment of $34,476.24, you still owe over $490,500! However if you pay $60,000 instead of $34,476.24, the interest component is still $25,000, 
+								so the remainder of your payment ($60,000-$25,000=$35,000) will go straight to the amount owing and after the first year you've paid off $35,000 and now have $465,000 
+								owing!</p>
+								<p>Let's say you have exactly the same situation as above, except you only have 5 years left. The amount you owe would be $149,264, and the interest component of your 
+								years payment of $34,476.24 is only $7,463. As the long progresses, you pay off the amount owing faster! What this means is, if you pay more than you're required to at 
+								the start of your loan, it will be significantly more effective in both reducing the time it will take you to pay back the loan and the total interest you pay! Free 
+								feel to play around with the calculator to see how paying extra will affect your loan.</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-variable-rate-payment-info').fadeToggle(200);">
+							<th>Your periodic variable rate payment</th>
+							<td id="results-variable-rate-payment"></td>
+						</tr>
+						<tr id="results-variable-rate-payment-info">
+							<td colspan="2">
+								<p>This is the value you will pay per week/month/fortnight IF this interest rate changes. This calculator lets you see what happens when a fixed interest rate switches to a 
+							variable rate (able to fluctuate).</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-extra-periodic-payment-info').fadeToggle(200);">
+							<th>Your extra periodic payment</th>
+							<td id="results-extra-periodic-payment"></td>
+						</tr>
+						<tr id="results-extra-periodic-payment-info">
+							<td colspan="2">
+								<p>This is the value of your on going periodic payment, PLUS the extra amount you've enter to pay. It is calculated by adding the value of your variable interest rate payment 
+								plus the extra amount. The higher this is, the faster you'll be able to pay off your loan.</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-adjusted-loan-duration-info').fadeToggle(200);">
+							<th>Your adjusted loan duration (Years)</th>
+							<td id="results-adjusted-loan-duration"></td>
+						</tr>
+						<tr id="results-adjusted-loan-duration-info">
+							<td colspan="2">
+								<p>The is the duration of your loan measured in years. If you choose $0 as your extra payment, the duration will be exactly the same as the time you choose. However with each 
+								additional dollar you choose to pay, the time it takes to pay back the loan will decrease.</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-total-payment-info').fadeToggle(200);">
+							<th>Your current total payment</th>
+							<td id="results-total-payment"></td>
+						</tr>
+						<tr id="results-total-payment-info">
+							<td colspan="2">
+								<p>This is the total money you will pay for the loan. It is calculated by adding the amount you chose to borrow, plus all the interest you pay on the loan.</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-new-total-payment-info').fadeToggle(200);">
+							<th>Your <em>new</em> total payment</th>
+							<td id="results-new-total-payment"></td>
+						</tr>
+						<tr id="results-new-total-payment-info">
+							<td colspan="2">
+								<p>This is the total value you will pay for your loan after being adjusted for your additional payment. The difference between this value and the value above is the total you 
+								save in interest payments!</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-interest-paid-without-info').fadeToggle(200);">
+							<th>Interest paid without your additional payment</th>
+							<td id="results-interest-paid-without"></td>
+						</tr>
+						<tr id="results-interest-paid-without-info">
+							<td colspan="2">
+								<p>This is the total value of the interest you pay for having the loan. Think of the interest payment as the cost of borrowing, or the cost of the loan. The instituion you 
+								borrowed from needs some incentive to offer you a loan, they also need to protect themselves from the time value of money. Remember what things cost 20 years ago? A dollar today 
+								is worth less than a dollar yesterday.</p>
+							</td>
+						</tr>
+						<tr onclick="$('#results-interest-saved-info').fadeToggle(200);">
+							<th>Interest saved with the additional payment</th>
+							<td id="results-interest-saved"></td>
+						</tr>
+						<tr id="results-interest-saved-info">
+							<td colspan="2">
+								<p>This is possibly the most important value the calculator provides, this is the total value of your savings by paying more each period. It may seem counter intuitive to think 
+								that paying more will somehow save you money. The explaination is that by paying more, you pay back the amount owing faster.</p>
+								<p>As you pay this value back quickly, the total duration of the loan decreases, you might be able to pay your loan back in 20 years instead of 25 years. If you're paying weekly, 
+								that would mean you just saved yourself 260 weeks of loan repayments!</p>
+							</td>
+						</tr>
+					</table>
+				</div>
+				<br /><br />
+				<div id="form-nav">
+					<div class="row">
+						<div class="col-md-6 half"><button type="button" class="btn btn-primary btn-large" onclick="previous();" id="previous-button" disabled="true">Previous</button></div>
+						<div class="col-md-6 half" style="text-align: right;"><button type="button" class="btn btn-success btn-large" onclick="next();" id="next-button">Next</button></div>
+					</div><br />
+					<div class="progress">
+						<div id="progress-bar" class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
+							<span class="sr-only">0% Complete</span>
+						</div>
 					</div>
 				</div>
 			</form>
@@ -126,10 +345,7 @@
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 		<script src="/bootstrap/js/ie10-viewport-bug-workaround.js"></script>
 
-		<!-- JavaScript for form -->
-		<script src="/scripts/loan-calculator.js" type="text/javascript"></script>
-
-		<!-- Bootstrap Dialog -->
+		<!-- Bootstrap Addons -->
 		<script src="/scripts/bootstrap-dialog.js" type="text/javascript"></script>
 	</body>
 </html>
--- Website/scripts/loan-calculator.js
+++ Website/scripts/loan-calculator.js
@@ -1,4 +1,115 @@
-var currentStep = 1, steps = null, progressBar = null, previousButton = null, nextButton = null;
+var currentStep = 0, steps = null, progressBar = null, previousButton = null, nextButton = null;
+
+function calculateResults() {
+	var paymentFrequency = parseFloat($("#payment-frequency")[0].value);
+	var borrowAmount = parseFloat($("#borrow-amount")[0].value);
+	var loanYears = parseFloat($("#loan-years")[0].value);
+	var fixedInterestYears = parseFloat($("#fixed-period")[0].value);
+	var fixedInterestRate = parseFloat($("#fixed-interest-rate")[0].value);
+	var loanLength = parseFloat($("#loan-length")[0].value);
+	var variableInterestRate = parseFloat($("#variable-interest-rate")[0].value);
+	var payAtEnd = parseFloat($("#pay-at-end")[0].value);
+
+	var rRateFx = fixedInterestRate / (paymentFrequency * 100);
+	var rDurati = loanLength * paymentFrequency
+	var stepOne = (rRateFx + 1);
+	var stepTwo = (Math.pow(stepOne, -rDurati));
+	var stepThr = (stepTwo - 1) * -1;
+	var stepFou = stepThr / rRateFx
+	var fixdPay = borrowAmount / stepFou
+	var fixdPyy = Math.round(fixdPay * 100) / 100;
+
+	if (loanYears > fixedInterestYears) {
+		var step111 = (loanLength - loanYears) * paymentFrequency
+		var step222 = (Math.pow(stepOne, -step111));
+		var step333 = (step222 - 1) * -1;
+		var step444 = step333 / rRateFx;
+		var pvCurLo = Math.round((step444 * fixdPay) * 100) / 100;
+
+		var rVarInt = variableInterestRate / (paymentFrequency * 100);
+		var type000 = (rVarInt + 1)
+		var type111 = (Math.pow(type000, -step111));
+		var type222 = (type111 - 1) * -1
+		var type333 = type222 / rVarInt
+		var newPAMT = pvCurLo / type333
+		var newPAM1 = Math.round(newPAMT * 100) / 100;
+
+		var newPay1 = newPAMT - (-payAtEnd);
+		var iTil111 = pvCurLo / newPay1;
+		var iTil222 = iTil111 * rVarInt;
+		var iTil333 = (iTil222 - 1) * -1;
+		var iTil444 = (Math.log(iTil333) / Math.log(type000)) * -1
+		var years11 = loanYears + (iTil444 / paymentFrequency);
+		var years12 = Math.round(years11 * 100) / 100;
+
+
+		var totPay1 = (paymentFrequency * loanYears * fixdPay) + (iTil444 * newPay1);
+		var totPy03 = Math.round(totPay1 * 100) / 100;
+		var totPay2 = Math.round((totPay1) * 1) / 1;
+		var totPay0 = (fixdPay * fixedInterestYears * paymentFrequency) + (loanLength - fixedInterestYears) * (paymentFrequency * newPAMT)
+		var intPay1 = totPay0 - borrowAmount;
+		var intPay3 = Math.round(intPay1 * 100) / 100;
+		var intPay2 = totPay0 - totPay1
+		var intPay4 = Math.round(intPay2 * 100) / 100;
+		var newPay2 = Math.round(newPay1 * 100) / 100;
+		var totPy01 = Math.round(totPay0 * 100) / 100;
+		if (payAtEnd === 0) {
+			intPay4 = 0
+		}
+		if (payAtEnd === 0) {
+			totPy03 = totPy01
+		}
+	} else {
+		var step111 = (loanLength - fixedInterestYears) * paymentFrequency
+		var step222 = (Math.pow(stepOne, -step111));
+		var step333 = (step222 - 1) * -1;
+		var step444 = step333 / rRateFx;
+		var pvCurLo = Math.round((step444 * fixdPay) * 100) / 100;
+
+		var rVarInt = variableInterestRate / (paymentFrequency * 100);
+		var type000 = (rVarInt + 1)
+		var type111 = (Math.pow(type000, -step111));
+		var type222 = (type111 - 1) * -1
+		var type333 = type222 / rVarInt
+		var newPAMT = pvCurLo / type333
+		var newPAM1 = Math.round(newPAMT * 100) / 100;
+
+		var newPay1 = newPAMT - (-payAtEnd);
+		var iTil111 = pvCurLo / newPay1;
+		var iTil222 = iTil111 * rVarInt;
+		var iTil333 = (iTil222 - 1) * -1;
+		var iTil444 = (Math.log(iTil333) / Math.log(type000)) * -1
+		var years11 = fixedInterestYears + (iTil444 / paymentFrequency);
+		var years12 = Math.round(years11 * 100) / 100;
+
+
+		var totPay1 = (paymentFrequency * fixedInterestYears * fixdPay) + (iTil444 * newPay1)
+		var totPy03 = Math.round(totPay1 * 100) / 100;
+		var totPay2 = Math.round((totPay1) * 1) / 1;
+		var totPay0 = (fixdPay * fixedInterestYears * paymentFrequency) + (loanLength - fixedInterestYears) * (paymentFrequency * newPAMT)
+		var intPay1 = totPay0 - borrowAmount;
+		var intPay3 = Math.round(intPay1 * 100) / 100;
+		var intPay2 = totPay0 - totPay1
+		var intPay4 = Math.round(intPay2 * 100) / 100;
+		var newPay2 = Math.round(newPay1 * 100) / 100;
+		var totPy01 = Math.round(totPay0 * 100) / 100;
+		if (payAtEnd === 0) {
+			intPay4 = 0
+		}
+		if (payAtEnd === 0) {
+			totPy03 = totPy01
+		}
+	}
+
+	$("#results-periodic-payment")[0].innerHTML = "$" + fixdPyy;
+	$("#results-variable-rate-payment")[0].innerHTML = "$" + newPAM1;
+	$("#results-extra-periodic-payment")[0].innerHTML = "$" + newPay2;
+	$("#results-adjusted-loan-duration")[0].innerHTML = years12;
+	$("#results-total-payment")[0].innerHTML = "$" + totPy01;
+	$("#results-new-total-payment")[0].innerHTML = "$" + totPy03;
+	$("#results-interest-paid-without")[0].innerHTML = "$" + intPay3;
+	$("#results-interest-saved")[0].innerHTML = "$" + intPay4;
+}
 
 function validateInput() {
 	$("main-form").submit();
@@ -23,33 +134,79 @@ function validateInput() {
 					message: 'Please enter a valid number representing how many years you have had the loan.'
 				});
 
+				return false;
+			}
+		break;
+		case 4:
+			if (!$("#fixed-interest-rate")[0].checkValidity() || $("#fixed-interest-rate")[0].value < 0 || 
+				$("#fixed-interest-rate")[0].value >= 100 || $("#fixed-interest-rate")[0].value == "") {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_DANGER,
+					title: 'Error',
+					message: 'Please enter a valid interest rate as a percentage.'
+				});
+
+				return false;
+			}
+		break;
+		case 5:
+			if (!$("#loan-length")[0].checkValidity() || $("#loan-length")[0].value <= 0 || $("#loan-length")[0].value == "") {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_DANGER,
+					title: 'Error',
+					message: 'Please enter a valid number representative of your loan\'s duration in years.'
+				});
+
+				return false;
+			}
+		break;
+		case 6:
+			if (!$("#variable-interest-rate")[0].checkValidity() || $("#variable-interest-rate")[0].value < 0 || 
+				$("#variable-interest-rate")[0].value >= 100 || $("#variable-interest-rate")[0].value == "") {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_DANGER,
+					title: 'Error',
+					message: 'Please enter a valid interest rate as a percentage.'
+				});
+
+				return false;
+			}
+		break;
+		case 7:
+			if (!$("#pay-at-end")[0].checkValidity() || $("#pay-at-end")[0].value <= 0) {
+				BootstrapDialog.show({
+					type: BootstrapDialog.TYPE_DANGER,
+					title: 'Error',
+		            message: 'Please enter a valid dollar amount representing how much money you wish to pay at the end of the fixed interest period.'
+		        });
+
 		        return false;
 			}
-			else {
-				BootstrapDialog.show({
-					type: BootstrapDialog.TYPE_SUCCESS,
-					title: 'Success',
-					message: 'Successfully completed the form!'
-		        });
-			}
 		break;
-		case 3:
-		break;
+	}
+
+	if (currentStep == steps.length) {
+		calculateResults();
 	}
 
 	return true;
 }
 
-function updateProgress() {
-	progressBar.style.width = (currentStep - 1) / steps.length * 100 + "%";
+function updateProgress(step) {
+	progressBar.style.width = (step - 1) / steps.length * 100 + "%";
 	
-	if (currentStep == steps.length) {
+	if (step == steps.length) {
 		$(previousButton).prop('disabled', false);
 		nextButton.innerHTML = "Finish";
 	}
-	else if (currentStep == 1) {
+	else if (step == 1) {
 		$(previousButton).prop('disabled', true);
 		nextButton.innerHTML = "Next";
+	}
+	else if (step > steps.length) {
+		$(previousButton).prop('disabled', true);
+		nextButton.innerHTML = "Restart";
+		nextButton.onclick = restart;
 	}
 	else {
 		$(previousButton).prop('disabled', false);
@@ -58,25 +215,76 @@ function updateProgress() {
 }
 
 function goToStep(stepNumber) {
-	$(steps[currentStep - 1]).fadeOut(200, function() {
-		$(steps[stepNumber - 1]).fadeIn(200);
-		currentStep = stepNumber;
-		updateProgress();
-	});
+	if (stepNumber > steps.length) {
+		$("#form-steps").fadeOut(200, function() {
+			$("#results").fadeIn(200);
+		});
+	}
+	else {
+		$(steps[currentStep - 1]).fadeOut(200, function() {
+			$(steps[stepNumber - 1]).fadeIn(200);
+			currentStep = stepNumber;
+		});
+	}
+			
+	$("body").animate(
+		{
+			scrollTop: 0
+		}, 
+			800, 
+			function(){
+				$("body").clearQueue();
+			}
+		);
+
+	updateProgress(stepNumber);
 }
+
+/*function focusNextField() {
+	var field = $(steps[currentStep - 1])[0].getElementsByTagName("input");
+
+	if (field != null) {
+		field[0].focus();
+	}
+}*/
 
 function previous() {
 	if (currentStep > 1) {
 		goToStep(currentStep - 1);
 	}
-
-	updateProgress();
 }
 
 function next() {
-	if (validateInput() && currentStep < steps.length) {
+	if (validateInput() && currentStep <= steps.length) {
 		goToStep(currentStep + 1);
 	}
+}
+
+function start() {
+	$("#welcome").fadeOut(200, function() {
+		$("#form-steps").fadeIn(200);
+		$("#form-nav").fadeIn(200);
+		currentStep = 1;
+		goToStep(1);
+	});
+}
+
+function restart() {
+	goToStep(1);
+
+	$("#results").fadeOut(200, function() {
+		$("#form-steps")[0].style.display = "block";
+		$("#main-form")[0].reset();
+	});
+
+	nextButton.onclick = next;
+	nextButton.innerHTML = "Next";
+}
+
+function checkForReturn(event) {
+	if(event.keyCode == 13){
+        next();
+    }
 }
 
 function prepare() {
@@ -87,7 +295,14 @@ function prepare() {
 
 	document.getElementById("main-form").submit = validateInput;
 
-	updateProgress();
+	updateProgress(0);
+
+
+    $('[data-toggle="popover"]').popover({
+    	title: "Information",
+        placement: "left",
+        html: true
+    });
 }
 
 window.onload = prepare;
